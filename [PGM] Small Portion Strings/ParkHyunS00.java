5ecb0bfaa037f8ffcaa4002dffa95951abd8de9f
ParkHyunSoo <parkhyuns00@naver.com>
2023-01-12 16:58:38 +0900

    크기가 작은 부분 문자열 문제 풀이

---
+++ [PGM] Small Portion Strings/ParkHyunS00.java
@@ -0,0 +1,34 @@
+class Solution1 {
+    public int solution(String t, String p) {
+        int answer = 0;
+        char[] t_arr = t.toCharArray();
+        long pNum = Long.parseLong(p);
+
+        for(int i=0; i<t.length() - p.length() + 1; i++){
+            String buff = "";
+            for(int j=i; j<i + p.length(); j++){
+                buff += t_arr[j];
+            }
+
+            long compareNum = Long.parseLong(buff);
+            if(compareNum <= pNum) ++answer;
+        }
+        return answer;
+    }
+}
+
+
+class Solution2 {
+    public int solution(String t, String p) {
+        int answer = 0;
+        int pNum = Integer.parseInt(p);
+
+        for(int i=0; i<t.length() - p.length() + 1; i++){
+            String buff = t.substring(i, i + p.length());
+            int compareNum = Integer.parseInt(buff);
+
+            if (compareNum <= pNum) ++answer;
+        }
+        return answer;
+    }
+}
