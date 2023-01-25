7934fa150c78dd3e3904795ff023a81c4e20ab8d
HwangSB <hsb990105@gmail.com>
2023-01-25 10:51:23 +0900

    다트 게임 문제 풀이

---
+++ [PGM] Dart Game/HwangSB.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+func solution(_ dartResult: String) -> Int {
+    var scores = Array(repeating: 0, count: 3)
+    
+    var index = 0
+    for result in dartResult {
+        switch result {
+        case "S":
+            index += 1
+        case "D":
+            scores[index] *= scores[index]
+            index += 1
+        case "T":
+            scores[index] *= scores[index] * scores[index]
+            index += 1
+        case "*":
+            scores[index-1] *= 2
+            if index >= 2 {
+                scores[index-2] *= 2
+            }
+        case "#":
+            scores[index-1] *= -1
+        default:
+            if scores[index] != 0 {
+                scores[index] = 10
+                continue
+            }
+            scores[index] = result.wholeNumberValue!
+        }
+    }
+    
+    return scores.reduce(0) {$0 + $1}
+}
